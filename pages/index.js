c14b9850f1355d917290f05c4c611120007789ed
blingboot <1979742455@qq.com>
2022-07-02 22:07:48 +0800

    fix: rm location

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-location.href = './real.html'
+// location.href = './real.html'
 
 export default function Home () {
   return (
